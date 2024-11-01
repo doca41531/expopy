93a9e018a2cf614d84aabaad6612dbc94b0eb1ed
doca41531 <doca41531@gmail.com>
2024-11-01 16:48:17 +0900

    24241

--- makejson.py
+++ makejson.py
@@ -7,10 +7,10 @@ import json
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 import requests
 import numpy as np
 from sklearn.linear_model import LinearRegression
-
 app = Flask(__name__)
 CORS(app)
 
@@ -26,7 +26,7 @@ def get_data():
 
     for i in range(10):
         # url = f"https://at.agromarket.kr/openApi/price/dateWhsalPumSale.do?serviceKey=D5C1EC499CB04B1295FBB05434FB3592&apiType=json&pageNo=1&strDate={int(datetime.today().strftime('%Y%m%d')) - i - 4}&endDate={int(datetime.today().strftime('%Y%m%d')) - i - 3}&large={Largevalue}&mid={midvalue}"
-        url = f"https://at.agromarket.kr/openApi/price/dateWhsalPumSale.do?serviceKey=FC139F4FF724467D9DEADEBE93E84FB7&apiType=json&pageNo=1&strDate={20240918 - i - 1}&endDate={20240918 - i}&large={Largevalue}&mid={midvalue}"
+        url = f"https://at.agromarket.kr/openApi/price/dateWhsalPumSale.do?serviceKey=BE89EA5AFF424DF4B2F73E4774EC242B&apiType=json&pageNo=1&strDate={20240918 - i - 1}&endDate={20240918 - i}&large={Largevalue}&mid={midvalue}"
         response = requests.get(url)
         contents = response.text
         json_ob = json.loads(contents)
@@ -38,6 +38,13 @@ def get_data():
     # 입력 데이터(X)와 출력 데이터(y) 준비
     X = np.array(range(len(arr))).reshape(-1, 1)  # 인덱스를 입력으로 사용
     y = np.array(arr)  # 리스트 값을 출력으로 사용
+    font_path = 'C:/Windows/Fonts/malgun.ttf'
+
+    # 폰트 이름 가져오기
+    font_name = fm.FontProperties(fname=font_path).get_name()
+
+    # 폰트 설정
+    plt.rc('font', family=font_name)
 
     # 선형 회귀 모델 학습
     model = LinearRegression()
@@ -59,18 +66,22 @@ def get_data():
 
     # 전체 꺾은선 그리기 (모든 점을 파란색으로)
     plt.plot(subset, marker='o', color='blue')
+    
+    for i, value in enumerate(subset):
+        plt.text(i, value + 500, f'{int(value)}', ha='center', fontsize=9)
 
     # 마지막 부분 강조 (마지막 점만 빨간색으로 표시)
     plt.plot(len(subset) - 1, subset[-1], marker='o', color='red', markersize=10)
+    plt.text(len(subset) - 1, subset[-1] - 500, "예측값", ha='center', fontsize=9)
 
     # y축 범위 설정
     plt.ylim(min(prearr) - 1000, max(prearr) + 1000)
 
     # 그래프 제목 및 축 레이블 설정
-    plt.title('Line Graph of 10 Data Points')
-    plt.xlabel('Index')
-    plt.ylabel('Value')
-
+    plt.title('최근 9일의 데이터와 다음날 예측 값')
+    plt.xlabel('일차')
+    plt.ylabel('원 / kg')
+    # plt.xticks(rotation=90)
     # 이미지를 메모리로 저장
     img = io.BytesIO()
     plt.savefig(img, format='png')
